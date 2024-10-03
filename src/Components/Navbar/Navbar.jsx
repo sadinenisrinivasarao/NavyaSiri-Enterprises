b3a1798e78757efcf3016ea6161ffa254cd4b398
Srinivasa Rao Sadineni <sadinenisrinivasarao6@gmail.com>
2024-10-02 21:18:08 -0500

    navbar responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
-// Import Link from both react-router-dom and react-scroll
 import { Link as RouterLink } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false); 
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); 
+  };
+
   return (
     <div className='navBar_div'>
       <h3 className='Company_name'>
         <RouterLink to="/">NavyaSiri Enterprises</RouterLink>
       </h3>
-      <nav>
+      <div className='menu-icon' onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <nav className={`nav_menu ${menuOpen ? 'active' : ''}`}>
         <ul className='nav_links'>
           <li>
-            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/" onClick={toggleMenu}>Home</RouterLink>
           </li>
           <li>
-            <RouterLink to="/about">About</RouterLink>
+            <RouterLink to="/about" onClick={toggleMenu}>About</RouterLink>
           </li>
           <li>
-            
-            <ScrollLink to="products" smooth={true} duration={500}>
+            <ScrollLink to="products" smooth={true} duration={500} onClick={toggleMenu}>
               Products
             </ScrollLink>
           </li>
           <li>
-            <RouterLink to="/contact">Contact</RouterLink>
+            <RouterLink to="/contact" onClick={toggleMenu}>Contact</RouterLink>
           </li>
         </ul>
       </nav>
