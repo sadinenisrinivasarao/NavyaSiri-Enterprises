362fd2cadc55771867497e1a77af79bda36b8d9a
Srinivasa Rao Sadineni <sadinenisrinivasarao6@gmail.com>
2024-10-03 11:34:21 -0500

    adding responsivenes

--- src/Components/Items/Item.jsx
+++ src/Components/Items/Item.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import "./item.css"
+import React from 'react';
+import './item.css';
 import { useNavigate } from 'react-router-dom';
 
-const Item = ({product}) => {
-    const navigate = useNavigate();
+const Item = ({ product }) => {
+  const navigate = useNavigate();
 
   const handleClick = () => {
     navigate(`/${product.sku}`);
   };
-  return (
-    <>
-    <div className="item">
-        <div className="item__image">
-            <img src={product.image} alt="item image"/>
-            
-        </div>
-        <div>
-            <h2 className="item__title">{product.name}</h2>
-            <p className='item_desc'>{product.description}</p>
-            <button className='know_more' onClick={handleClick} >Know more</button>
-        </div>
-    </div>
-    </>
-  )
-}
 
-export default Item
+  return (
+    <div className="item">
+      <div className="item__image">
+        <img src={product.image} alt="item image" />
+      </div>
+      <h2 className="item__title">{product.name}</h2>
+      <div className="overlay">
+        <h2 className="overlay__title">{product.name}</h2>
+        <p className="overlay__desc">{product.description}</p>
+        <button className="know_more" onClick={handleClick}>Know more</button>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
