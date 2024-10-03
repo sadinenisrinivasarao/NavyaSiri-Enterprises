018ce4d6df261b3bd7ce9487613382371ddd51d8
Srinivasa Rao Sadineni <sadinenisrinivasarao6@gmail.com>
2024-10-03 12:53:24 -0500

    CSS update

--- src/Pages/ProductPage/Productpage.jsx
+++ src/Pages/ProductPage/Productpage.jsx
@@ -1,7 +1,7 @@
 import  { React, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { products } from '../../assets/assets'; 
-
+import "./ProductPage.css"
 const ProductPage = () => {
   useEffect(() => {
     window.scrollTo(0, 0); 
